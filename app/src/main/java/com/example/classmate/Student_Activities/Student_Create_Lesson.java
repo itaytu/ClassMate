95bae09bae45d4b9f71c9dac22689899f983c4e3
moshe <moshelh95@gmail.com>
2020-01-08 15:20:45 +0200

    add to date and time

--- app/src/main/java/com/example/classmate/Student_Activities/Student_Create_Lesson.java
+++ app/src/main/java/com/example/classmate/Student_Activities/Student_Create_Lesson.java
@@ -1,9 +1,16 @@
 package com.example.classmate.Student_Activities;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TimePicker;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -24,9 +31,14 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Objects;
 
-public class Student_Create_Lesson extends AppCompatActivity {
+public class Student_Create_Lesson extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnDatePicker, btnTimePicker;
+    private EditText txtDate, txtTime;
+    private int mYear, mMonth, mDay, mHour, mMinute;
 
     private FirebaseAuth fAuth;
     private FirebaseFirestore fStore;
@@ -41,6 +53,14 @@ public class Student_Create_Lesson extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student__create__lesson);
+
+        btnDatePicker=findViewById(R.id.btn_date);
+        btnTimePicker=findViewById(R.id.btn_time);
+        txtDate=findViewById(R.id.in_date);
+        txtTime=findViewById(R.id.in_time);
+
+        btnDatePicker.setOnClickListener(this);
+        btnTimePicker.setOnClickListener(this);
 
         fAuth = FirebaseAuth.getInstance();
         fStore = FirebaseFirestore.getInstance();
@@ -97,4 +117,50 @@ public class Student_Create_Lesson extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onClick(View v) {
+        if (v == btnDatePicker) {
+
+            // Get Current Date
+            final Calendar c = Calendar.getInstance();
+            mYear = c.get(Calendar.YEAR);
+            mMonth = c.get(Calendar.MONTH);
+            mDay = c.get(Calendar.DAY_OF_MONTH);
+
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                    new DatePickerDialog.OnDateSetListener() {
+
+                        @Override
+                        public void onDateSet(DatePicker view, int year,
+                                              int monthOfYear, int dayOfMonth) {
+
+                            txtDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+                        }
+                    }, mYear, mMonth, mDay);
+            datePickerDialog.show();
+        }
+        if (v == btnTimePicker) {
+
+            // Get Current Time
+            final Calendar c = Calendar.getInstance();
+            mHour = c.get(Calendar.HOUR_OF_DAY);
+            mMinute = c.get(Calendar.MINUTE);
+
+            // Launch Time Picker Dialog
+            TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                    new TimePickerDialog.OnTimeSetListener() {
+
+                        @Override
+                        public void onTimeSet(TimePicker view, int hourOfDay,
+                                              int minute) {
+
+                            txtTime.setText(hourOfDay + ":" + minute);
+                        }
+                    }, mHour, mMinute, false);
+            timePickerDialog.show();
+        }
+    }
 }
+
