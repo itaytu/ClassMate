7730626e277b16b1a16c01f79cbd9ad771bfe76e
Itay Tuson <itaytu@wix.com>
2020-01-14 13:52:30 +0200

    disabled swiping tabs.

--- app/src/main/java/com/example/classmate/Student_Activities/Student_HomePage.java
+++ app/src/main/java/com/example/classmate/Student_Activities/Student_HomePage.java
@@ -12,8 +12,8 @@ import com.example.classmate.R;
 import com.example.classmate.Student_Activities.Fragments.home_page;
 import com.example.classmate.Student_Activities.Fragments.lessons;
 import com.example.classmate.Student_Activities.Fragments.requests;
+import com.example.classmate.Utils.CustomViewPager;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.tabs.TabLayout;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,7 +21,7 @@ import java.util.List;
 public class Student_HomePage extends AppCompatActivity {
 
     private fragment_adapter fragment_adapter;
-    private ViewPager viewPager;
+    private CustomViewPager viewPager;
     private BottomNavigationView botNav;
 
     @Override
@@ -30,6 +30,7 @@ public class Student_HomePage extends AppCompatActivity {
         setContentView(R.layout.activity_home_page);
 
         viewPager = findViewById(R.id.fragment_container);
+        viewPager.addOnPageChangeListener(onPageChangeListener);
 
         botNav = findViewById(R.id.bottom_navigation);
         botNav.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
@@ -43,43 +44,14 @@ public class Student_HomePage extends AppCompatActivity {
         viewPager.setAdapter(fragment_adapter);
         viewPager.setCurrentItem(1);
         botNav.setSelectedItemId(R.id.nav_home);
-        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-                switch (position) {
-                    case 0:
-                        botNav.getMenu().findItem(R.id.nav_lessons).setChecked(true);
-                        break;
-                    case 1:
-                        botNav.getMenu().findItem(R.id.nav_home).setChecked(true);
-                        break;
-                    case 2:
-                        botNav.getMenu().findItem(R.id.nav_requests).setChecked(true);
-                        break;
-                }
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-
-            }
-        });
-
     }
 
 
     private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-//            Fragment selectedFragment = new Fragment();
 
             switch (menuItem.getItemId()){
-
                 case R.id.nav_lessons:
                     viewPager.setCurrentItem(0);
                     break;
@@ -91,9 +63,34 @@ public class Student_HomePage extends AppCompatActivity {
                 case R.id.nav_home:
                     viewPager.setCurrentItem(1);
             }
-
             return true;
         }
     };
+
+
+    private ViewPager.OnPageChangeListener onPageChangeListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
+
+        @Override
+        public void onPageSelected(int position) {
+            switch (position) {
+                case 0:
+                    botNav.getMenu().findItem(R.id.nav_lessons).setChecked(true);
+                    break;
+                case 1:
+                    botNav.getMenu().findItem(R.id.nav_home).setChecked(true);
+                    break;
+                case 2:
+                    botNav.getMenu().findItem(R.id.nav_requests).setChecked(true);
+                    break;
+            }
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {}
+    };
+
+
 
 }
